8f8dff3bcd62edf43b6350a6392e5f73f6b25616
Luiz Junior <luizjrdeveloper@gmail.com>
2019-05-23 10:56:20 -0300

    Fix.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         if (env('APP_ENV') === 'production') {
-            URL::forceSchema('https');
+            URL::forceScheme('https');
         }
     }
 }
