e6ef3089c45b5c6db0b6378e43285e92430551f1
Luiz Silva <luizjrdeveloper@gmail.com>
2019-05-23 19:47:38 -0300

    Começando a criar as tabelas

---
+++ database/migrations/2019_05_23_224310_create_produtos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->string('descricao');
+            $table->string('preco');
+            $table->integer('quantidade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
