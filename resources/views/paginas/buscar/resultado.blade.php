d1996e56c4d5a8a0f34c79c4454d7ea72084e976
Luiz Junior <luizjrdeveloper@gmail.com>
2019-05-24 18:15:55 -0300

    Att.

--- resources/views/paginas/buscar/resultado.blade.php
+++ resources/views/paginas/buscar/resultado.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
-@section('title', __('Cadastrar Categoria'))
+@section('title', __('Resultado da Busca'))
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="mb-6">
-                <h2>Resultado da Busca</h2>
+                <h2>{{ __('Resultado da Busca') }}</h2>
             </div>
 
             <div class="card-columns">
@@ -15,11 +15,12 @@
                     <div class="card-body">
                         <h5 class="card-title">{{ $produto->nome }}</h5>
                         <p class="card-text">{{ $produto->descricao }}</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <p class="card-text"><a href=""><small>{{ $produto->categoria->nome }}</small></a></p>
+                        <a href="#" class="btn btn-primary">{{ __('Ver Produto') }}</a>
                     </div>
                 </div>
                 @endforeach
-                
+
             </div>
         </div>
     </div>
