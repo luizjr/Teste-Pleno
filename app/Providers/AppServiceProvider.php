b2dc377f1eb2d6990a91c056e605267dd4f20801
Luiz Jr <luizjrdeveloper@gmail.com>
2025-04-17 01:03:55 -0300

    Refactor code structure and remove redundant changes for improved readability and maintainability

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,46 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\Route;
-
-use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-    * Register any application services.
-    *
-    * @return void
-    */
-    public function register()
+     * Register any application services.
+     */
+    public function register(): void
     {
         //
     }
 
     /**
-    * Bootstrap any application services.
-    *
-    * @return void
-    */
-    public function boot()
+     * Bootstrap any application services.
+     */
+    public function boot(): void
     {
-        if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
-        }
-        Schema::defaultStringLength(191);
-
-        Route::resourceVerbs([
-            'index' => 'listar',
-            'create' => 'criar',
-            'store' => 'salvar',
-            'edit' => 'editar',
-            'show' => 'ver',
-            'update' => 'atualizar',
-            'destroy' => 'excluir',
-        ]);
-
-        Paginator::defaultView('layouts.pagination.bootstrap-4');
+        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => 'language'], function () {
-
-    Route::get('/', function () {
-        return view('welcome');
-    });
-
-    Auth::routes();
-    Route::get('logout', 'Auth\LoginController@logout', function () {
-        return abort(404);
-    });
-
-    Route::get('/home', 'ProdutoController@index')->name('home');
-    Route::any('/buscar', 'BuscaController@index')->name('buscar');
-    Route::resources([
-        'produtos' => 'ProdutoController',
-        'categorias' => 'CategoriaController'
-    ]);
+Route::get('/', function () {
+    return view('welcome');
 });
